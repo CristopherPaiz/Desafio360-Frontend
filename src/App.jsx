93805c16b28085791efe0913648d83bd5784ec22
CristopherPaiz <cpaizl1@miumg.edu.gt>
2024-12-27 10:20:58 -0600

    Refactor App component to include routing and authentication; add ProtectedRoute for secure access to Home and Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import ProtectedRoute from "./layouts/ProtectedRoute";
+
 import LoginPage from "./Views/LoginPage";
+
+const Home = () => <h1>Página de Inicio</h1>;
+const Dashboard = () => <h1>Dashboard</h1>;
 
 const App = () => {
   return (
-    <div>
-      <LoginPage />
-    </div>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 };
 
