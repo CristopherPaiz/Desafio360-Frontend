fb153601466d73a9d2df2d54ac2563a8adc1099b
CristopherPaiz <cpaizl1@miumg.edu.gt>
2024-12-28 12:14:59 -0600

    Add ProductosPorCategoria

--- src/Views/HomePage.jsx
+++ src/Views/HomePage.jsx
@@ -1,10 +1,12 @@
 import CategoriasBarra from "../components/CategoriasBarra/CategoriasBarra";
+import ProductosPorCategoria from "../components/ProductosPorCategorias/ProductosPorCategoria";
 
 const HomePage = () => {
   return (
     <>
       {/* BARRA DE CATEGORIAS */}
       <CategoriasBarra />
+      <ProductosPorCategoria />
     </>
   );
 };
